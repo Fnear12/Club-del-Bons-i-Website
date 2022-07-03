0f810e9c5b050fe2ac696dfa72e1401e96c48496
Fnear <carlosmiramartinez@gmail.com>
2022-07-03 14:29:30 +0200

    menu closed as default

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 const navLinks = document.getElementById("navLinks")
 
 function showMenu() {
-  navLinks.className = navLinks.className.replace(' hidden', '')
+  navLinks.className = navLinks.className + ' show'
 }
 
 function hideMenu() {
-  navLinks.className = navLinks.className + ' hidden'
+  navLinks.className = navLinks.className.replace(' show', '')
 }
